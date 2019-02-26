fcdb3180d79db06bb0a8e6570a4fb4bf28631cdb
GMRun100 <734122618@qq.com>
2019-02-26 19:10:20 +0100

    add useStruct/useStructDlg.cp

--- useStruct/useStructDlg.cpp
+++ useStruct/useStructDlg.cpp
@@ -13,6 +13,51 @@
 
 
 // 用于应用程序“关于”菜单项的 CAboutDlg 对话框
+#include <iostream>
+
+#include <cstring>
+
+using namespace std;
+struct Books
+{
+	char title[50];
+	char author[50];
+	char subject[100];
+	int book_id;
+};
+//一下为1000的代码部分
+typedef unsigned char		__u8;
+typedef unsigned short		__u16;
+typedef unsigned int		__u32;
+typedef unsigned __int64 __u64;
+typedef __u8	f8_u8;
+typedef __u16 f8_u16;
+typedef __u32 f8_u32;
+typedef __u64 f8_u64;
+typedef __u64 f8_uuid;
+
+struct mem_addr_t {
+	f8_u16	addr;
+	f8_u8	section;
+	f8_u8	padding_0;
+}PACKED;
+
+struct var_def_t {
+	f8_uuid uuid;//f8_vm
+	struct mem_addr_t addr;//f8_binary
+};
+
+struct X {
+	char a;			// 1 bytes
+	char padding1[3];	// 3 bytes
+	float b;		// 4 bytes
+	int c;			// 4 bytes
+	char padding2[4];	// 4 bytes
+	double d;		// 8 bytes
+	unsigned e;		// 4 bytes
+	char padding3[4];	// 4 bytes
+};
+
 
 class CAboutDlg : public CDialogEx
 {
@@ -99,6 +144,53 @@ BOOL CuseStructDlg::OnInitDialog()
 	SetIcon(m_hIcon, FALSE);		// 设置小图标
 
 	// TODO: 在此添加额外的初始化代码
+
+	Books book1;
+	strcpy_s(book1.title, "C++教程");
+	strcpy_s(book1.author, "benliu");
+	strcpy_s(book1.subject, "编程语言");
+	book1.book_id = 12345;
+
+	//输出book1的信息
+	cout << "第一本书的标题" << book1.title << endl;
+	cout << "第一本书的作者" << book1.author << endl;
+	cout << "第一本书的编程语言" << book1.subject << endl;
+	cout << "第一本书的id" << book1.book_id << endl;
+
+	char BinaryAddress[64];
+	var_def_t * def;
+	for(int i = 0; i < 64; i++)
+	{
+		BinaryAddress[i] = '\0';
+	}
+	BinaryAddress[0] = '?';
+	BinaryAddress[1] = '?';
+	BinaryAddress[2] = '\x10';
+	BinaryAddress[3] = '[';
+	BinaryAddress[4] = '}';
+	BinaryAddress[5] = '?';
+	BinaryAddress[6] = '\f';
+	BinaryAddress[7] = '?';
+	BinaryAddress[8] = '\f';
+	BinaryAddress[9] = '\0';
+	BinaryAddress[10] = '\x2';
+
+
+	def = (var_def_t*)BinaryAddress;
+	int n_mem_addr_t = sizeof(mem_addr_t);
+	int n_var_def_t = sizeof(var_def_t);
+	int n_def = sizeof(def);
+	int n_BinaryAddress = sizeof(BinaryAddress);
+	
+	f8_uuid m_64 = def->uuid;
+	
+	f8_u16	m_addr= def->addr.addr;
+	f8_u8	m_section=def->addr.section;
+	f8_u8	m_padding_0=def->addr.padding_0;
+
+	int n_X = sizeof(X);
+
+
 
 	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
 }
